82fb0022687c7d26faebe56a3f586d27f546c7f1
Christopher Deutsch <cd@cdeutsch.com>
2013-09-25 14:31:47 -0700

    Sample web app that will launch the native barcode scanner if accessed via the native Xamarin app.

---
+++ Xamarin.Web.BarcodeSample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xamarin.Web.BarcodeSample.Models;
+
+namespace Xamarin.Web.BarcodeSample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var model = new PromoModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PromoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: save to DB, or execute other business logic.
+
+                return View("Success", model);
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Xamarin.Web.BarcodeSample/Models/PromoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Xamarin.Web.BarcodeSample.Models
+{
+    public class PromoModel
+    {
+        [Required]
+        public string PromoCode { get; set; }
+    }
+}
